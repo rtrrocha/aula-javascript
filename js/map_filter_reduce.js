d82c1987d4c3366a9126df7f4e67db9ae4bdf99d
Ricardo T Rocha <rtr.rocha@gmail.com>
2020-08-31 00:49:44 -0300

    Finalizando a aula de map-filter-reduce

--- js/map_filter_reduce.js
+++ js/map_filter_reduce.js
@@ -1,3 +1,5 @@
+// USANDO O FILTER()
+
 //Exemplo for removendo duplicados
 /*
 for (var i =0; i< Array.length; i++)
@@ -22,19 +24,111 @@ var uniqueProducts = array.filter(function(elem, i, array)
 // var newArray = arr.filter(callback[thisarg])
 const pets = [
     {
-        nome:    'pondera',
+        nome: 'pondera',
         especie: 'cão',
-        idade:    10
+        idade: 10,
+        peso: 23.4
     },
     {
-        nome:    'luke',
+        nome: 'luke',
         especie: 'gato',
-        idade:    6
+        idade: 6,
+        peso: 6.7
     },
     {
-        nome:    'nemo',
+        nome: 'nemo',
         especie: 'peixe',
-        idade:    1
+        idade: 1,
+        peso: 0.2
+    },
+    {
+        nome: 'Lilika',
+        especie: 'cão',
+        idade: 13,
+        peso: 25.3
+    },
+    {
+        nome: 'bituca',
+        especie: 'cão',
+        idade: 11,
+        peso: 11.2
     },
 ];
+
+//Função dentro da execução
+/*
+const newPets = pets.filter((pet) => {
+    return pet.idade < 5;
+})
+*/
+// Chamando a função externa
+const MenorQ5 = (numero) => {
+    return numero < 7;
+}
+
+const newPets = pets.filter(({ idade }) => MenorQ5(idade));
+
 console.log(pets);
+console.log(newPets);
+// FIM EXEMPLO FILTER()  //
+
+// USANDO O MAP()  //
+//Chama o callback para cada elemento e devolde um 
+// novo array com a mesma quantidade de itens
+// var newArray = arr.map(callback,[ this Arg])
+
+const petNomes = pets.map((pet) => {
+    if (pet.nome == "bituca"){
+        return pet.nome = "bolinha";
+    }
+    else {
+        return pet.nome;
+    }
+    
+
+})
+console.log(petNomes);
+
+// USANDO O REDUCE()    //
+// Executa uma função para cada elemento 
+// retornando um único valor de retorno
+// Retorando o total do objeto pesos do array
+const TotalPeso = pets.reduce((total, pet) => {
+    return total + pet.peso;
+}, 0);
+console.log("Total: "+ TotalPeso);
+
+// Retorando o total do objeto idade do array
+const TotalIdade = pets.reduce((total, pet) => {
+    return total + pet.idade;
+}, 0);
+console.log("Total: "+ TotalIdade);
+
+// Unificando as duas funções acima em um único REDUCE
+// const TotalPesoIdade  = pets.reduce((total, pet) => {
+//     return {
+//         SomaIdade: total.SomaIdade + pet.idade,
+//         SomaPeso: total.SomaPeso + pet.peso
+//     }
+// }, {SomaIdade: 0, SomaPeso: 0})
+// console.log(TotalPesoIdade);
+
+// Condicionando a espécie cão
+const TotalPesoIdadeDogs  = pets.reduce((total, pet) => {
+    if (pet.especie === "cão"){
+        return {
+            SomaIdade: total.SomaIdade + pet.idade,
+            SomaPeso: total.SomaPeso + pet.peso,
+            TipoEspecie: pet.especie
+        };
+    }
+    else {
+        return {
+            SomaIdade: total.SomaIdade,
+            SomaPeso: total.SomaPeso
+        };
+    }
+
+
+}, {SomaIdade: 0, SomaPeso: 0, TipoEspecie: "" })
+console.log(TotalPesoIdadeDogs);
